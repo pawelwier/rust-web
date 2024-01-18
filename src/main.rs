4631f0630ecc046bedb6d07b9604cf7334c366eb
pawel <pawiercinski@gmail.com>
2024-01-18 22:11:49 +0100

    add serde, post endpoint

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,33 @@
-#[macro_use]
-extern crate rocket;
+use rocket::{
+    serde::{
+        Deserialize, json::Json
+    }, 
+    get,
+    post, 
+    build, 
+    launch,
+    routes
+};
+
+#[derive(Deserialize)]
+pub struct Task<'r> {
+    pub description: &'r str,
+    pub complete: bool,
+    pub points: i32
+}
 
 #[get("/")]
 fn index() -> &'static str {
     "Hello, from Rocket!"
 }
 
+#[post("/add", data = "<task>", format = "json")]
+fn add_task(task: Json<Task<'_>>) {
+    println!("{}", task.description);
+}
+
 #[launch]
 fn rocket() -> _ {
-    rocket::build().mount("/", routes![index])
+    build()
+        .mount("/", routes![index, add_task])
 }

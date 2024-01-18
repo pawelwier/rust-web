098bf77e5f3d40ac71a5a90f857d6d2536418e18
pawel <pawiercinski@gmail.com>
2024-01-18 23:18:46 +0100

    post routes, res status

--- src/main.rs
+++ src/main.rs
@@ -2,11 +2,13 @@ use rocket::{
     serde::{
         Deserialize, json::Json
     }, 
+    response::status,
     get,
     post, 
     build, 
     launch,
-    routes
+    routes, 
+    Responder
 };
 
 #[derive(Deserialize)]
@@ -16,18 +18,35 @@ pub struct Task<'r> {
     pub points: i32
 }
 
+#[derive(Responder)]
+#[response(status = 222, content_type = "json")]
+pub struct TaskResponseJson(String);
+
 #[get("/")]
 fn index() -> &'static str {
     "Hello, from Rocket!"
 }
 
-#[post("/add", data = "<task>", format = "json")]
-fn add_task(task: Json<Task<'_>>) {
+#[post("/add/<id>", data = "<task>", format = "json")]
+fn add_task(id: usize, task: Json<Task<'_>>) -> status::Accepted<String> {
     println!("{}", task.description);
+    status::Accepted(format!("id: {}", id))
+}
+
+#[post("/add-task", data="<task>", format="json")]
+fn add_task_res(task: Json<Task<'_>>) -> TaskResponseJson {
+    let complete = if task.complete { "yes" } else { "no" };
+    let response_str: String = format!(
+        "{{\"name\": \"{}\",\"name\": \"{}\",\"points\": \"{}\"}}", 
+        task.description, complete, task.points
+    );
+    TaskResponseJson(response_str)
 }
 
 #[launch]
 fn rocket() -> _ {
     build()
-        .mount("/", routes![index, add_task])
+        .mount("/", routes![
+            index, add_task, add_task_res
+        ])
 }
